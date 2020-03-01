30e792e44552201a6660d69aaf8ad4fef2fcf23e
Mateusz <mrpeciak@gmail.com>
2020-03-01 20:07:20 +0100

    C1: add index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+function imagesToggle() {
+	const images = document.querySelectorAll('img');
+	let counter = 0;
+	document.addEventListener('keydown', (e) => {
+		if (e.keyCode === 88) {
+			images[counter].scrollIntoView()
+			counter++
+		} else if (e.keyCode === 90) {
+			counter--
+			images[counter].scrollIntoView()
+		}
+	})
+}
+
+function imagesToggleTimer() {
+	const images = document.querySelectorAll('img');
+	let counter = 0;
+		setInterval(() => {
+			counter++
+			images[counter].scrollIntoView()
+		})	
+}
